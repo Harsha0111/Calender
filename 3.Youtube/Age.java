e172af8de87406a257c89edc83f4ac3bb6602789
Harsha0111 <selviharsha@gmail.com>
2022-09-20 15:21:31 +0530

    NotePad

--- 3.Youtube/Age.java
+++ 3.Youtube/Age.java
@@ -1,6 +1,8 @@
 import java.util.Date;
 import java.awt.*;
 import java.util.Arrays;
+import java.text.NumberFormat;
+import java.util.Scanner;
 
 public class Age {
  public static void main(String args[]) {
@@ -129,12 +131,84 @@ public class Age {
 	 
 	double g = 1.1;
 	double h = g +2 ;
+	//int h = (int)g + 2;
 
 	System.out.println(h );
 	
 
+	//String non-primitive
 
+	//String i = "1";
+	//Integer(i)
+	//int j = Integer.parseInt(i) +2;
+
+	String k = "1.1";
+	// double /float  
+	double l = Double.parseDouble(k) +2;
 	
+	System.out.println(l);
+
+	  
+	//Math class
+
+	//round
+	//int result4 = Math.round (1.1F);
+	
+	// ceil and casting
+ 	//int result4 = (int) Math.ceil(1.1F);
+
+	//floor
+	//int result4 = (int) Math.floor(1.1F);
+
+	// min,max -> int 
+	//int result4 = Math.min(1,2);
+
+	//random value -> double o/p:14.677889908
+	//double result4 = Math.random() *100;
+
+	// casting implicit -> rounding the o/p value 99
+	//double result4 =Math.round(Math.random() *100);
+	
+	//explicit casting , removing Math.round and (int) Math.random() *100 = 0 only if we use wrap na only random value will generate
+	double result4 =(int)(Math.random() *100);
+	
+	System.out.println(result4);
+ 
+
+	//Formating Number and import 
+
+	// NumberFormat its Attributre so, obj/instance can't created so get methods are available
+ 	
+	//Currency, using getCurrencyInstance() method
+  	//NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+	// String for $ symbol and format method is used
+	//String result5 = currency.format(654243.4562);
+	
+	//Percentage, using getPercentInstance() method
+  	//NumberFormat percent = NumberFormat.getPercentInstance();
+
+	// format method is used
+	//String result5 = percent.format(654243.4562);
+
+	//method chaining
+	//(NumberFormat percent ) cutting this portion and mixing  get format in same position= NumberFormat.getPercentInstance();
+
+	String result5 =NumberFormat.getPercentInstance().format(0.9); ;
+
+	System.out.println(result5);
+	
+	//import Scanner
+	//Reading I/P
+	Scanner scanner = new Scanner(System.in);
+	//System.out.println("Enter Age");
+
+	// float,double,.. -> nextFloat,..like wise 
+	//float age1 = scanner.nextFloat();
+
+	//but for string only next -> 1 token ( Mosh )and nextLine na it comes -> 2 tokens(Mosh Ala ) also as result 
+	String age1 = scanner.next();
+	System.out.println("AGE"+ age1);
 
 	}
 }
